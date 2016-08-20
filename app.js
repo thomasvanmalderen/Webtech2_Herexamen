dedb1ac3f01f66d8e48f3d960ba944796d4a036f
Thomas Van malderen <thomas.vanmalderen@student.thomasmore.be>
2016-08-20 15:49:55 +0200

    working on code cleanup

--- app.js
+++ app.js
@@ -1,102 +1,80 @@
+// WEBTECH 2 Herexamen (Thomas Van Malderen)  - IMD FOOTBALL
+
+//REQUIRE MODULES
 var express = require('express');
 var app = express();
 var md5 = require('js-md5');
-
-//SOCKET IO
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-
 var controller = require('./controllers/matches');
-
-//BODYPARSER
 var bodyParser = require('body-parser');
-
-//MONGOOSE
 var mongoose = require('mongoose');
+
+//CONNECT TO DATABASE
 mongoose.connect('mongodb://localhost:27017/Football');
 
-// parse application/json , define this BEFORE adding routes
-app.use(bodyParser.json());
 
-//PUG
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
 
-//USE IMAGES ETC
-app.use(express.static('public'));
-app.use(express.static('files'));
-app.use('/static', express.static('public'));
 
 app.use('/', require('./routers/index'));
 app.use('/matches', require('./routers/matches'));
 
 //ALL SOCKETS
 io.on('connection', function(socket){
-  socket.on("New Match", function(newMatch){
-      controller.create(newMatch, function(returnMatch){
+    socket.on("New Match", function(newMatch){
+        controller.create(newMatch, function(returnMatch){
         console.log(returnMatch);
         io.emit("newMatchInDB", returnMatch);
-      });
-  });
+        });
+    });
   
     //COUNTRY 1 GOALS
     socket.on("c1plusgoal", function(updateMatch){
-      controller.c1plusGoal(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c1plusGoal(updateMatch, function(returnMatch){
         io.emit("addedc1goal", returnMatch);
-      });
-  });
+        });
+    });
     socket.on("c1minusgoal", function(updateMatch){
-      controller.c1minusGoal(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c1minusGoal(updateMatch, function(returnMatch){
         io.emit("removedc1goal", returnMatch);
-      });
-  });
+        });
+    });
     //COUNTRY 2 GOALS
     socket.on("c2plusgoal", function(updateMatch){
-      controller.c2plusGoal(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c2plusGoal(updateMatch, function(returnMatch){
         io.emit("addedc2goal", returnMatch);
       });
-  });
+    });
     socket.on("c2minusgoal", function(updateMatch){
-      controller.c2minusGoal(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c2minusGoal(updateMatch, function(returnMatch){
         io.emit("removedc2goal", returnMatch);
-      });
-  });
+        });
+    });
     
-    
-    
-    //COUNTRY 1 shots
+    //COUNTRY 1 SHOTS
     socket.on("c1plusshot", function(updateMatch){
-      controller.c1plusShot(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c1plusShot(updateMatch, function(returnMatch){
         io.emit("addedc1shot", returnMatch);
-      });
-  });
+        });
+    });
     socket.on("c1minusshot", function(updateMatch){
-      controller.c1minusShot(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c1minusShot(updateMatch, function(returnMatch){
         io.emit("removedc1shot", returnMatch);
-      });
-  });
+        });
+    });
     //COUNTRY 2 SHOTS
     socket.on("c2plusshot", function(updateMatch){
-      controller.c2plusShot(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c2plusShot(updateMatch, function(returnMatch){
         io.emit("addedc2shot", returnMatch);
-      });
-  });
+        });
+    });
     socket.on("c2minusshot", function(updateMatch){
-      controller.c2minusShot(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.c2minusShot(updateMatch, function(returnMatch){
         io.emit("removedc2shot", returnMatch);
-      });
-  });
-    
-    
-    
+        });
+    });
     
     //COUNTRY 1 FOULS
     socket.on("c1plusfoul", function(updateMatch){
@@ -125,43 +103,32 @@ io.on('connection', function(socket){
       });
   });
     
+    //MATCH COMMENTARY
     socket.on("updatecommentary", function(updateMatch){
-      controller.updateCommentary(updateMatch, function(returnMatch){
-        //console.log(returnMatch);
+        controller.updateCommentary(updateMatch, function(returnMatch){
         io.emit("updatedcommentary", returnMatch);
-      });
-  });
+        });
+    });
     
     socket.on("unlock", function(tohash){
-        //alert("working unlock");
-        console.log("geweldig");
-        
+        console.log("unlock");
         console.log(tohash);
-        //console.log(md5());
        if(md5(tohash) == '8d9f1681272ed981448e1afa0dc92335'){
-           //alert("unlocked");
            console.log("unlocked");
            io.emit("unlockedmanage");
        }
-        
-      
-  });
+    });
     socket.on("unlockcreate", function(tohash2){
-        //alert("working unlock");
-        console.log("geweldig");
-        
+        console.log("unlocking");
         console.log(tohash2);
-        //console.log(md5());
        if(md5(tohash2) == '8d9f1681272ed981448e1afa0dc92335'){
-           //alert("unlocked");
            console.log("unlocked");
            io.emit("unlockedmanage2");
        }
-        
-      
-  });
+    });
 });
 
+// APP ACTIVATION
 http.listen(3000, function(){
-  console.log('App working on port *:3000');
+  console.log('App working on port 3000');
 });
